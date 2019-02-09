18caac745a829ff6142b010979fc8fc68a251327
lorenzo <lorenzoiapadre@icloud.com>
2019-02-09 12:14:47 +0100

    modifica font bottoni e nuova formattazione tabella gestione eventi

--- Studio_Calvarese/resources/views/services/gestioneEventi.blade.php
+++ Studio_Calvarese/resources/views/services/gestioneEventi.blade.php
@@ -9,27 +9,28 @@
     <br>
 
     <div class="table-wrapper">
-        <table>
+        <table style="border-collapse:separate">
             <thead>
             <tr>
                 <th>Nome Evento</th>
                 <th>Categoria</th>
                 <th>Data</th>
-                <th>Gestione</th>
+                <th>Impaginati</th>
+                <th>Stampe</th>
+                <th>Pubblicazione</th>
             </tr>
             </thead>
             <tbody>
-            <tr>
-                @foreach($events as $event)
-                <td>{{$event->titolo}}</td>
-                <td>{{$event->categoria}}</td>
-                    <td>{{date('d F Y',strtotime($event->giorno))}}</td>
-                <td><a href="#"class="button"><span class="icon fas fa-address-book"></span> Impaginato</a>
-                    <a href="#"class="button"><span class="icon fas fa-image"></span> Stampe</a>
-                    <a href="#"class="button primary"><span class="icon fas fa-align-center"> </span> Rendi pubblico</a>
-                </td>
-            </tr>
-            @endforeach
+                <tr>
+                    @foreach($events as $event)
+                        <td>{{$event->titolo}}</td>
+                        <td>{{$event->categoria}}</td>
+                        <td>{{date('d F Y',strtotime($event->giorno))}}</td>
+                        <td><a href="#"class="button"><span class="icon fas fa-address-book"></span> Impaginato</a></td>
+                        <td><a href="#"class="button"><span class="icon fas fa-image"></span> Stampe</a></td>
+                        <td><a href="#"class="button primary"><span class="icon fas fa-align-center"> </span> Rendi pubblico</a></td>
+                </tr>
+                    @endforeach
             </tbody>
         </table>
     </div>
